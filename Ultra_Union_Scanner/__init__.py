945fdbfe8b7afdec65b04dc355c274c9e312a3b1
『itzzzyashu™』 <83384892+itzzzyashu@users.noreply.github.com>
2022-08-23 14:31:24 +0530

    v1.0.0

--- Ultra_Union_Scanner/__init__.py
+++ Ultra_Union_Scanner/__init__.py
@@ -1,4 +1,3 @@
-"""Gets ENV vars or Config vars then calls class."""
 
 import traceback
 import logging
@@ -13,27 +12,44 @@ from motor import motor_asyncio
 from datetime import datetime
 
 
-
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
-    level=logging.INFO,
-)
+    level=logging.INFO)
 
-# config
-API_ID_KEY = ""
-API_HASH_KEY = ""
-STRING_SESSION = ""
-BOT_TOKEN = ""
-Skynet = [2053172802,1969730847]
-ENFORCERS = [1969730847, 2088713608, 2089452706, 1118151835]
-INSPECTORS = [1969730847, 2088713608, 2089452706, 1118151835]
-MONGO_DB_URL = ""
-Skynet_logs = "SanatanRakshaGlobalLogs" # SRS • Global
-Skynet_approved_logs = "SanatanRakshaApprovalLogs" # SRS • Approvals
-GBAN_MSG_LOGS = -1001708065341 # Counter-49(Gban Origination Group)
+ENV = bool(os.environ.get("ENV", True))
+if ENV:
+    API_ID_KEY = int(os.environ.get("API_ID_KEY"))
+    API_HASH_KEY = os.environ.get("API_HASH_KEY")
+    STRING_SESSION = os.environ.get("STRING_SESSION")
+    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY")
+    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
+    RAW_Skynet = os.environ.get("Skynet", "")
+    RAW_ENFORCERS = os.environ.get("ENFORCERS", "")
+    Skynet = [int(x) for x in os.environ.get("Skynet", "").split()]
+    INSPECTORS = [int(x) for x in os.environ.get("INSPECTORS", "").split()]
+    ENFORCERS = [int(x) for x in os.environ.get("ENFORCERS", "").split()]
+    MONGO_DB_URL = os.environ.get("MONGO_DB_URL")
+    Skynet_logs = int(os.environ.get("Skynet_logs"))
+    Skynet_approved_logs = int(os.environ.get("Skynet_approved_logs"))
+    GBAN_MSG_LOGS = int(os.environ.get("GBAN_MSG_LOGS"))
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+else:
+    import Skynet_System.config as Config
 
-
+    API_ID_KEY = Config.API_ID
+    API_HASH_KEY = Config.API_HASH
+    STRING_SESSION = Config.STRING_SESSION
+    MONGO_DB_URL = Config.MONGO_DB_URL
+    with open(os.path.join(os.getcwd(), "Skynet_System/elevated_users.json"), "r") as f:
+        data = json.load(f)
+    Skynet = data["Skynet"]
+    ENFORCERS = data["ENFORCERS"]
+    INSPECTORS = data["INSPECTORS"]
+    Skynet_logs = Config.Skynet_logs
+    Skynet_approved_logs = Config.Skynet_approved_logs
+    GBAN_MSG_LOGS = Config.GBAN_MSG_LOGS
+    BOT_TOKEN = Config.BOT_TOKEN
 
 INSPECTORS.extend(Skynet)
 ENFORCERS.extend(INSPECTORS)
@@ -57,26 +73,27 @@ async def make_collections() -> str:
     if (
         await collection.count_documents({"_id": 1}, limit=1) == 0
     ):  # Blacklisted words list
-        dictw = {"_id": 1}
-        dictw["blacklisted"] = []
+        dictw = {"_id": 1, "blacklisted": []}
         await collection.insert_one(dictw)
 
     if (
         await collection.count_documents({"_id": 2}, limit=1) == 0
     ):  # Blacklisted words in name list
-        dictw = {"_id": 2, "Type": "Wlc Blacklist"}
-        dictw["blacklisted_wlc"] = []
+        dictw = {"_id": 2, "Type": "Wlc Blacklist", "blacklisted_wlc": []}
         await collection.insert_one(dictw)
     if await collection.count_documents({"_id": 3}, limit=1) == 0:  # Gbanned users list
-        dictw = {"_id": 3, "Type": "Gban:List"}
-        dictw["victim"] = []
-        dictw["gbanners"] = []
-        dictw["reason"] = []
-        dictw["proof_id"] = []
+        dictw = {
+            "_id": 3,
+            "Type": "Gban:List",
+            "victim": [],
+            "gbanners": [],
+            "reason": [],
+            "proof_id": [],
+        }
+
         await collection.insert_one(dictw)
     if await collection.count_documents({"_id": 4}, limit=1) == 0:  # Rank tree list
-        sample_dict = {"_id": 4, "data": {}, "standalone": {}}
-        sample_dict["data"] = {}
+        sample_dict = {"_id": 4, "standalone": {}, "data": {}}
         for x in Skynet:
             sample_dict["data"][str(x)] = {}
             sample_dict["standalone"][str(x)] = {

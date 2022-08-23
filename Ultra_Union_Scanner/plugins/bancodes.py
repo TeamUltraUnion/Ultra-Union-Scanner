c26343e5a34eea94589454e07b658cadee417b63
『itzzzyashu™』 <83384892+itzzzyashu@users.noreply.github.com>
2022-08-23 12:16:40 +0530

    Update and rename Sanatan_Raksha_System/plugins/bancodes.py to Ultra_Union_Scanner/plugins/bancodes.py

---
+++ Ultra_Union_Scanner/plugins/bancodes.py
@@ -0,0 +1,26 @@
+   
+help_plus = """ Here is Help for **Bancodes** :-
+**•** `{UUSx00}` **SCAMMER.**
+**•** `{UUSx01}` **SPAM ADDING MEMBER**
+**•** `{UUSx03}` **ABUSE SPAM**
+**•** `{UUSx04}` **NSFW SPAMMER**
+**•** `{UUSx06}` **IMPERSONATION**
+**•** `{UUSx07}` **MD/BTC SCAM**
+**•** `{UUSx08}` **ADDING SPAMBOTS**
+**•** `{UUSx10}` **ILLEGAL**
+**•** `{UUSx11}` **PHISHING**
+**•** `{UUSx12}` **FRAUD PROMOTION  [ANY KIND]**
+**•** `{UUSx13}` **CYBER THREATENING / CYBER BULLY**
+**•** `{UUSx14}` **CHILD ABUSE**
+**•** `{UUSx15}` **BAN EVASION**
+**•** `{UUSx16}` **SPAMBOT**
+**•** `{UUSx17}` **RAID INITIALIZOR** 
+**•** `{UUSx18}` **RAID PARTICIPANT**
+**•** `{UUSx19}` **KRIMINALANT**
+**•** `{UUSx20}` **SPAMMING VIOLENT CONTENT**
+**•** `{UUSx21}` **HATE SPEECH AGAINST NATIONALITY**
+**•** `{UUSx22}` **RAID/SPAM INFLAMMER**
+**•** `{UUSx23}` **PRONOGRAPHY CCONTENT PROMOTING**
+**•** `{UUSx24}` **PAID GIRL/BOY**
+"""
+__plugin_name__ = "bancodes"
